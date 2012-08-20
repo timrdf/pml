8e1e015ef92d1c6a07b97d331a2ba60933c97a62
Timothy Lebo <timrdf@gmail.com>
2012-08-20 15:32:59 -0400

    added prov ns

--- doc/html-exemplars/refresh.sh
+++ doc/html-exemplars/refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-stub-ontology-exemplars.py https://raw.github.com/timrdf/pml/master/ontology/pml-3.0.owl http://provenanceweb.org/ns/pml# pml
-#$DATAFAQS_HOME/bin/stub-ontology-exemplars.py                                    ../../ontology/pml-3.0.owl http://provenanceweb.org/ns/pml# pml
+ $DATAFAQS_HOME/bin/stub-ontology-exemplars.py https://raw.github.com/timrdf/pml/master/ontology/pml-3.0.owl http://provenanceweb.org/ns/pml# pml prov
+#$DATAFAQS_HOME/bin/stub-ontology-exemplars.py                                    ../../ontology/pml-3.0.owl http://provenanceweb.org/ns/pml# pml prov
 
 # https://github.com/timrdf/DataFAQs/blob/master/bin/stub-ontology-exemplars.py
