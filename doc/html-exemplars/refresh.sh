54e48dd713c00547219ad4261676d48b4c272642
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-08-20 13:28:31 -0600

    pml3 rdf

--- doc/html-exemplars/refresh.sh
+++ doc/html-exemplars/refresh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
- $DATAFAQS_HOME/bin/stub-ontology-exemplars.py https://raw.github.com/timrdf/pml/master/ontology/pml-3.0.owl http://provenanceweb.org/ns/pml# pml
+stub-ontology-exemplars.py https://raw.github.com/timrdf/pml/master/ontology/pml-3.0.owl http://provenanceweb.org/ns/pml# pml
 #$DATAFAQS_HOME/bin/stub-ontology-exemplars.py                                    ../../ontology/pml-3.0.owl http://provenanceweb.org/ns/pml# pml
